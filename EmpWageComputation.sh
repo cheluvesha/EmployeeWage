38c492b3d2f26890392e0b24264fa5e2201e9d62
cheluvesha <cheluvesh.b@gmail.com>
2020-07-10 14:04:39 +0530

    Daily and total wage is added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,17 +1,13 @@
 #!/bin/bash -x
 isparttime=1;
 isfulltime=2;
-max_Hrs_Mon=10;
+max_Hrs_Mon=4;
 emprateperHr=20;
 numworkingdays=20;
 totalEmpHr=0;
 totalworkingdays=0;
-while [[ $totalEmpHr -lt $max_Hrs_Mon &&
-         $totalworkingdays -lt $numworkingdays ]]
-do
-  ((totalworkingdays++))
-        empcheck=$(( RANDOM%3 ))
-        case $empcheck in
+function getworkinghrs() {
+ case $1 in
                 $isfulltime)
                         empHrs=8
                                 ;;
@@ -22,9 +18,20 @@ do
                         empHrs=0
                                 ;;
         esac
-        totalEmpHr=$(($totalEmpHr*$empHrs))
+}
+function calDailywage() {
+ local empHrs=$1;
+ wage=$(($empHrs*$emprateperHr))
+}
+while [[ $totalworkingHrs -lt $max_Hrs_Mon && $totalworkingdays -lt $numworkingdays ]]
+do
+  ((totalworkingdays++))
+        empHrs="$( getworkinghrs $((RANDOM%3)) )"
+        totalworkingHrs=$(( $totalworkingHrs * $empHrs ))
+        empwage[$totalworkingdays]+"$( calDailywage $empHrs )"
 done
-totalSalary=$(($totalEmpHr*$emprateperHr))
+totalSalary=$(( calDailywage $totalworkingHrs ))
+echo "Daily wage" ${empwage[@]}
 
 
 
