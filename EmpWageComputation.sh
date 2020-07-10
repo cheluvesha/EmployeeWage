4dba1173925ab746abb73e84bee47c98e28a9767
cheluvesha <cheluvesh.b@gmail.com>
2020-07-10 13:53:20 +0530

    Wages for month is Added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,19 +1,25 @@
 #!/bin/bash -x
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-empCheck=$((RANDOM%3));
-case $empCheck in
-        $isFullTime)
-                empHrs=8;
-                        ;;
-        $isPartTime)
-                empHrs=4;
-                        ;;
+isparttime=1;
+isfulltime=2;
+totalsalary=0;
+emprateperHr=20;
+numworkingdays=20;
+for (( day=1; day<=$numworkingdays; day++ ))
+do
+empcheck=$(( RANDOM%3 ))
+        case $empcheck in
+                $isfulltime)
+                        empHrs=8
+                                ;;
+                $isparttime)
+                        empHrs=4
+                                ;;
                 *)
-                empHrs=0;
-                        ;;
-esac
-salary=$(($empHrs*$empRatePerHr));
-
-
+                        empHrs=0
+                                ;;
+        esac
+        salary=$(( $empHrs*$emprateperHr ));
+        totalsalary=$(( $totalsalary+$salary ));
+done
+echo $salary
+echo $totalsalary
