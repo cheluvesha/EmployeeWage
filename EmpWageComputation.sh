5dcc2763e990d37617432c0ef8458dc2e3c2574d
cheluvesha <cheluvesh.b@gmail.com>
2020-07-10 14:08:16 +0530

    Daily and total wage is added to Dictionary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,37 +1,47 @@
 #!/bin/bash -x
-isparttime=1;
-isfulltime=2;
-max_Hrs_Mon=4;
-emprateperHr=20;
-numworkingdays=20;
-totalEmpHr=0;
-totalworkingdays=0;
-function getworkinghrs() {
- case $1 in
-                $isfulltime)
-                        empHrs=8
-                                ;;
-                $isparttime)
-                        empHrs=4
-                                ;;
+return_value=0
+function get_working_hrs() {
+        local emp_check=$1
+        local full_time=1;
+        local part_time=2;
+        case $emp_check in
+                $full_time)
+                        return_value=8
+                                        ;;
+                $part_time)
+                        return_value=4
+                                        ;;
                 *)
-                        empHrs=0
-                                ;;
+                        return_value=0
+                                        ;;
         esac
+    return
 }
-function calDailywage() {
- local empHrs=$1;
- wage=$(($empHrs*$emprateperHr))
-}
-while [[ $totalworkingHrs -lt $max_Hrs_Mon && $totalworkingdays -lt $numworkingdays ]]
-do
-  ((totalworkingdays++))
-        empHrs="$( getworkinghrs $((RANDOM%3)) )"
-        totalworkingHrs=$(( $totalworkingHrs * $empHrs ))
-        empwage[$totalworkingdays]+"$( calDailywage $empHrs )"
-done
-totalSalary=$(( calDailywage $totalworkingHrs ))
-echo "Daily wage" ${empwage[@]}
+function get_emp_wage() {
+        local working_hrs=$1;
+        dailywage=$((working_hrs*rate_hor))
 
+echo $dailywage
+}
+number_of_days=5
+rate_hor=20
+days=0
+total_hrs=0
+declare -A daily_salary
+while [ $days -lt $number_of_days ]
+do
+        is_present=$((RANDOM%3))
+        get_working_hrs $is_present
+        working_hrs=$return_value
+        total_hrs=$((total_hrs+working_hrs))
+        daily_salary["day $days"]="$( get_emp_wage $working_hrs )"
+        ((days++))
+done
+total_salary=$((total_hrs*rate_hor))
+echo $total_salary
+for i in "${!daily_salary[@]}"
+do
+        echo $i"-" ${daily_salary[$i]}
+done
 
 
