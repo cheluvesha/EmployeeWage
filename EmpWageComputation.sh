c5797048460e5d8a5a926f90dab91b6c94057456
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 13:15:51 +0530

    Part time Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,4 +1,27 @@
 #!/bin/bash -x
+#UC1
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
+
+#UC2
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
+else
+        salary=0;
+fi
+
+#UC3
 isPartTime=1;
 isFullTime=2;
 empRatePerHr=20;
