16be58703a50acfa5dd71d327a2d2fc64a772ec1
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 13:26:23 +0530

    Storing in Dictionary

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,4 +1,177 @@
 #!/bin/bash -x
+#UC1
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
+
+#UC2
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
+else
+        salary=0;
+fi
+
+#UC3
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+if [ $isFullTime -eq $randomCheck ]
+then
+        empHrs=8;
+elif [ $isPartTime -eq $randomCheck ]
+then
+        empHrs=4;
+else
+        salary=0;
+fi
+salary=$salary=$(($empHrs*$empRatePerHr));
+
+#UC4
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3));
+case $empCheck in
+        $isFullTime)
+                empHrs=8;
+                        ;;
+        $isPartTime)
+                empHrs=4;
+                        ;;
+                *)
+                empHrs=0;
+                        ;;
+esac
+salary=$(($empHrs*$empRatePerHr));
+
+#UC5
+isparttime=1;
+isfulltime=2;
+totalsalary=0;
+emprateperHr=20;
+numworkingdays=20;
+for (( day=1; day<=$numworkingdays; day++ ))
+do
+empcheck=$(( RANDOM%3 ))
+        case $empcheck in
+                $isfulltime)
+                        empHrs=8
+                                ;;
+                $isparttime)
+                        empHrs=4
+                                ;;
+                *)
+                        empHrs=0
+                                ;;
+        esac
+        salary=$(( $empHrs*$emprateperHr ));
+        totalsalary=$(( $totalsalary+$salary ));
+done
+echo $salary
+echo $totalsalary
+#UC6
+isparttime=1;
+isfulltime=2;
+max_Hrs_Mon=10;
+emprateperHr=20;
+numworkingdays=20;
+totalEmpHr=0;
+totalworkingdays=0;
+while [[ $totalEmpHr -lt $max_Hrs_Mon &&
+         $totalworkingdays -lt $numworkingdays ]]
+do
+  ((totalworkingdays++))
+        empcheck=$(( RANDOM%3 ))
+        case $empcheck in
+                $isfulltime)
+                        empHrs=8
+                                ;;
+                $isparttime)
+                        empHrs=4
+                                ;;
+                *)
+                        empHrs=0
+                                ;;
+        esac
+        totalEmpHr=$(($totalEmpHr*$empHrs))
+done
+totalSalary=$(($totalEmpHr*$emprateperHr))
+
+#UC7
+isparttime=1;
+isfulltime=2;
+max_Hrs_Mon=10;
+emprateperHr=20;
+numworkingdays=20;
+totalEmpHr=0;
+totalworkingdays=0;
+while [[ $totalEmpHr -lt $max_Hrs_Mon &&
+         $totalworkingdays -lt $numworkingdays ]]
+do
+  ((totalworkingdays++))
+        empcheck=$(( RANDOM%3 ))
+        case $empcheck in
+                $isfulltime)
+                        empHrs=8
+                                ;;
+                $isparttime)
+                        empHrs=4
+                                ;;
+                *)
+                        empHrs=0
+                                ;;
+        esac
+        totalEmpHr=$(($totalEmpHr*$empHrs))
+done
+totalSalary=$(($totalEmpHr*$emprateperHr))
+
+#UC8
+isparttime=1;
+isfulltime=2;
+max_Hrs_Mon=4;
+emprateperHr=20;
+numworkingdays=20;
+totalEmpHr=0;
+totalworkingdays=0;
+function getworkinghrs() {
+ case $1 in
+                $isfulltime)
+                        empHrs=8
+                                ;;
+                $isparttime)
+                        empHrs=4
+                                ;;
+                *)
+                        empHrs=0
+                                ;;
+        esac
+}
+function calDailywage() {
+ local empHrs=$1;
+ wage=$(($empHrs*$emprateperHr))
+}
+while [[ $totalworkingHrs -lt $max_Hrs_Mon && $totalworkingdays -lt $numworkingdays ]]
+do
+  ((totalworkingdays++))
+        empHrs="$( getworkinghrs $((RANDOM%3)) )"
+        totalworkingHrs=$(( $totalworkingHrs * $empHrs ))
+        empwage[$totalworkingdays]+"$( calDailywage $empHrs )"
+done
+totalSalary=$(( calDailywage $totalworkingHrs ))
+echo "Daily wage" ${empwage[@]}
+
+#UC9
 return_value=0
 function get_working_hrs() {
         local emp_check=$1
@@ -43,5 +216,3 @@ for i in "${!daily_salary[@]}"
 do
         echo $i"-" ${daily_salary[$i]}
 done
-
-
