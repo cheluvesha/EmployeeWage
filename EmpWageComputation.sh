f44759299c70881eb158a31a6ddec2e554a30ed2
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 13:15:03 +0530

    Daily employee wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,4 +1,15 @@
-#!/bin/bash -x
+#!/bin/bash
+#UC1
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
+
+#UC2
 isPresent=1;
 randomCheck=$((RANDOM%2));
 if [ $isPresent -eq $randomCheck ]
