1159ffe9c0e38f82025030b00da1804d57bfd671
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-07-15 13:18:45 +0530

    for total working hours or day reached

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,4 +1,85 @@
 #!/bin/bash -x
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+        echo "Employee is present"
+else
+        echo "Employee is absent"
+fi
+
+#UC2
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ]
+then
+        empRatePerHr=20;
+        empHrs=8;
+        salary=$(($empHrs*$empRatePerHr));
+else
+        salary=0;
+fi
+
+#UC3
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+randomCheck=$((RANDOM%3));
+if [ $isFullTime -eq $randomCheck ]
+then
+        empHrs=8;
+elif [ $isPartTime -eq $randomCheck ]
+then
+        empHrs=4;
+else
+        salary=0;
+fi
+salary=$salary=$(($empHrs*$empRatePerHr));
+
+#UC4
+isPartTime=1;
+isFullTime=2;
+empRatePerHr=20;
+empCheck=$((RANDOM%3));
+case $empCheck in
+        $isFullTime)
+                empHrs=8;
+                        ;;
+        $isPartTime)
+                empHrs=4;
+                        ;;
+                *)
+                empHrs=0;
+                        ;;
+esac
+salary=$(($empHrs*$empRatePerHr));
+
+#UC5
+isparttime=1;
+isfulltime=2;
+totalsalary=0;
+emprateperHr=20;
+numworkingdays=20;
+for (( day=1; day<=$numworkingdays; day++ ))
+do
+empcheck=$(( RANDOM%3 ))
+        case $empcheck in
+                $isfulltime)
+                        empHrs=8
+                                ;;
+                $isparttime)
+                        empHrs=4
+                                ;;
+                *)
+                        empHrs=0
+                                ;;
+        esac
+        salary=$(( $empHrs*$emprateperHr ));
+        totalsalary=$(( $totalsalary+$salary ));
+done
+echo $salary
+echo $totalsalary
+#UC6
 isparttime=1;
 isfulltime=2;
 max_Hrs_Mon=10;
